4297069fd166452e1bc29d02a8397f075da2ea7a
Sal Tamay <sal.tamay@outlook.com>
2019-11-17 14:39:39 -0500

    Get user input and github profile details GitHub user profile details being obtained through github api

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const inquirer = require('inquirer');
 const axios = require('axios');
 
 
+const generateDeveloperProfile = async () => {
+
+    try {
+
+        // Get user's github profile handle and their favorite color
+        const userInput =
+            await inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "Enter your GitHub handle: ",
+                        name: "login"
+                    },
+                    {
+                        type: "input",
+                        message: "Wha is your favorite color? ",
+                        name: "color"
+                    }
+                ]);
+
+        let userDetails = await axios.get(`https://api.github.com/users/${userInput.login}`);
+
+        userDetails = {
+            ...userInput,
+            ...userDetails.data
+        };
+
+        console.log(userDetails);
+
+    } catch (error) {
+
+    }
+
+}
+
+generateDeveloperProfile();
